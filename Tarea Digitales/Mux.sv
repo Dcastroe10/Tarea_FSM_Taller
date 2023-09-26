9cc17760d75e13481e67b842076467d6c696a9cf
Dcastroe10 <85363712+Dcastroe10@users.noreply.github.com>
2023-09-25 18:22:10 -0600

    Delete Tarea Digitales directory

--- Tarea Digitales/Mux.sv
+++
@@ -1,9 +0,0 @@
-module Mux (
-    input wire A,
-    input wire B,
-    input wire S,
-    output wire Y
-);
-    // Lógica del multiplexor
-    assign Y = (S == 1'b0) ? A : B;
-endmodule
--- Tarea Digitales/comparador.sv
+++
@@ -1,7 +0,0 @@
-module comparador (
-    input [7:0] entrada,
-    output igual
-);
-
-assign igual = (entrada == 8'hC8);
-endmodule
